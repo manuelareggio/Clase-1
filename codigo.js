6b7e7171ee284dea5e7bb3167795d81c5ad6a78d
manuelareggio <mareggio@itba.edu.ar>
2022-04-12 20:59:11 -0300

    b

---
+++ codigo.js
@@ -0,0 +1,38 @@
+// Resolver problemas logicos o amtematicos//
+// La programacion es una herramienta para resolver problemas//
+// Descomosicion: descomponer el problema//
+// Patrones: similitudes//
+// Abstraccion: achicar el problema, eliminar lo superfluo//
+// Algoritmos: pasos para resolver el problema//
+
+console.log("Hola mundo")
+document.write('La Web de Manu');
+
+/*
+Variable: Espacio en memoria donde se almacena un valor.
+Que es? Un cajon.
+como armo un avariable?
+let nombreDeLaCVaribale = "valor"
+let nombre = "manu"
+= es el operador de asignacion
+var << para crear variable 
+
+Tipos de datos: 
+numerico: enteros (integers) o con coma (float)
+strings: cadena de caracteres
++ > suma o concatena (el + no es solo para los numeros, tambien esx para strings)
+en la suma, el string tiene mas peso que un valor numerico 
+con las "" es una cadena de caracteres 
+Nan: Not a Number
+JV tiene por convencion 
+camel Case: primera letra de la primera palabra minus, primera letra segunda palabra mayus.
+snake case: usar guin bajo entre palabras
+kebbab case: usar guin medio entre palabras
+pascal case: primera letra de la primera y segunda palabra en mayus
+
+= asigna un valor a una variable 
+== pregunta si el valor coinciden 
+=== pregunta si el valor y el tipo de dato coincide
+nosotros vamos a usar siempre el triple = 
+no se usa elif como en phyton, se usa else if 
+*/
