04f7f3a5ed67891b9c9448ca20d47ed5e389fcc8
manuelareggio <mareggio@itba.edu.ar>
2022-06-21 19:14:26 -0300

    b

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-console.log("Soy externo")
+console.log("Soy externo");
 //Para acceder a un Id
-let parrafo1 = document.getElementById("pConId")
-console.log(parrafo1)
+let parrafo1 = document.getElementById("pConId");
+console.log(parrafo1);
 
-//Para acceder a una clase 
-console.log(parrafo1)
-let parrafo2 = document.getElementsByClassName("pConClass")
-console.log(parrafo2)
+//Para acceder a una clase
+console.log(parrafo1);
+let parrafo2 = document.getElementsByClassName("pConClass");
+console.log(parrafo2);
 
-//Para acceder a una etiqueta 
-let parrafo3 = document.getElementsByTagName("p")
-console.log(parrafo3)
+//Para acceder a una etiqueta
+let parrafo3 = document.getElementsByTagName("p");
+console.log(parrafo3);
